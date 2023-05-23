b269534b72034e42d435837060fcfcbe8c7a0121
B1RDOW <griner710@gmail.com>
2023-05-24 02:03:32 +0600

    Исправления

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<footer className="border-t py-3 border-zinc-100 dark:border-zinc-800 bg-white dark:bg-zinc-900/80 backdrop-blur-xl backdrop-saturate-150">
 			<div className="container px-6 mx-auto grid grid-flow-col gap-x-3 justify-center items-center font-medium">
-				<Link href="https://github.com/B1RDOW" className="text-teal-700 hover:text-teal-600 transition mt-0.5" target="_blank" rel="noreferrer noopener">GitHub</Link>
+				<Link href="https://github.com/B1RDOW/omsk-streets" className="text-teal-700 hover:text-teal-600 transition" target="_blank" rel="noreferrer noopener">Исходный код</Link>
 				<p className="text-teal-700 hover:text-teal-600 transition select-none">|</p>
 				<Link href="https://vanilla-plus.ru" className="text-teal-700 hover:text-teal-600 transition" target="_blank" rel="noreferrer noopener">Другие проекты</Link>
 			</div>
